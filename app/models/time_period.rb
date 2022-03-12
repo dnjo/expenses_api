b8bd2c25ad2e8d7d8af321b47ec1cca716629184
Dennis Johansson <dennis@dnjo.net>
2022-03-12 19:12:03 +0100

    Add time period duplication

--- app/models/time_period.rb
+++ app/models/time_period.rb
@@ -1,4 +1,17 @@
 class TimePeriod < ApplicationRecord
   has_many :expense_statuses, dependent: :delete_all
   belongs_to :user
+
+  def create_from(year:, month:)
+    new_period = TimePeriod.create(year: year, month: month, user: self.user)
+    new_period.expense_statuses << expense_statuses.map do |status|
+      new_status = status.dup
+      new_status.paid = false
+      new_status.save!
+      new_status
+    end
+
+    new_period.save!
+    new_period
+  end
 end
